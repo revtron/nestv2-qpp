d2364c4cae6677c3337a53d3b51ce753448cda65
Ubuntu <ubuntu@ip-172-31-21-218.ec2.internal>
2026-01-15 18:09:19 +0000

    fix: add missing CRUD methods to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -16,7 +17,23 @@ export class UsersService {
   }
 
   findAll() {
-    // This tells NestJS to also bring the related tasks!
     return this.usersRepository.find({ relations: ['tasks'] });
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOne({ 
+      where: { id }, 
+      relations: ['tasks'] 
+    });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.usersRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number) {
+    await this.usersRepository.delete(id);
+    return { deleted: true };
   }
 }
